adbac4bce344bc6646320cd258b0e9873413ef99
Yogev Heskia <yogev911@gmail.com>
2019-08-11 15:21:35 +0300

    added value type for each metric

--- services/horizon/internal/actions_metrics.go
+++ services/horizon/internal/actions_metrics.go
@@ -3,7 +3,7 @@ package horizon
 import (
 	"github.com/kinecosystem/go/services/horizon/internal/actions"
 	"github.com/kinecosystem/go/support/render/hal"
-	"github.com/rcrowley/go-metrics"
+	metrics "github.com/rcrowley/go-metrics"
 )
 
 // Interface verification
